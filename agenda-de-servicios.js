d6de7303b152a942d926beac731bcb36e03ad0f5
smonsalvo <sergio.monsalvo.s@gmail.com>
2023-05-29 22:00:22 -0300

    arreglando codigo parra vaciar carrito al agendar el servicio

--- agenda-de-servicios.js
+++ agenda-de-servicios.js
@@ -1,26 +1,52 @@
-//creo la clase para construir los servicios a ofrecer
 
-class Servicio {
-    constructor(id, tipoDeServicio, preciodeServicio, img) {
-        this.id = id;
-        this.tipoDeServicio = tipoDeServicio;
-        this.preciodeServicio = preciodeServicio;
-        this.img = img;
-        this.cantidad = 1;
-    }
+
+//Cargo mis Servicios desde archivo JSON usando fetch
+
+const listado = document.getElementById ("listado");
+
+const listaServicios = "json/listaServicios.json";
+
+let servicios = [];
+
+
+//creo la funcion para mostrar mis servicios
+const mostrarServicio = () => {
+
+fetch (listaServicios)
+        .then (respuesta => respuesta.json ())
+        .then (datos => {
+            servicios = [datos];
+            console.log (servicios);
+            datos.forEach (servicio => {
+                listado.innerHTML += `
+                <div class="card"> 
+                    <img class="card-img-top imgImagenServicio img-fluid" src="${servicio.img}" alt="${servicio.tipoDeServicio}">
+                    <div class="card-body">
+                         <h3>${servicio.tipoDeServicio}</h3>
+                         <p>$${servicio.preciodeServicio}</p>
+                        <button class="btn btn-primary" id="boton${servicio.id}">Agregar al Carrito</button>
+                    </div>
+                </div>
+                                        
+                `
+                
+            // Agregar servicios al carrito
+        const boton = document.getElementById(`boton${servicio.id}`);
+        boton.addEventListener("click", () => {
+            agregarACarrito(servicio.id);
+
+            //una vez seleccionado el boton se desactiva
+            boton.disabled = true;
+            boton.styleopacity = 0.95;
+        })
+            
+            }
+                )
+
+        })
+
+        
 }
-
-const cambioDeAceite = new Servicio(1, "Cambio de Aceite y Filtro", 22900, "img/cambio-de-aceite-2.jpg");
-const cambioDePastillasDeFrenos = new Servicio(2, "Cambio de Pastillas de Frenos", 20900, "img/cambio-de-frenos.avif");
-const alineacionYBalanceo = new Servicio(3, "Alineación y Balanceo", 8900, "img/alineacion-balanceo.avif");
-const rotacionDeNeumaticos = new Servicio(4, " Rotación de Neumaticos", 4490, "img/rotacion-neumaticos-1.jpg");
-const inspeccionComputarizada = new Servicio(5, "Inspección Integral por Computadora", 15499, "img/revision-integral.jpg");
-const serviceAnual15MKm = new Servicio(6, "Service Anual de 15 mil Km", 47900, "img/revision-integral-siniestro.jpg");
-
-
-//Declaro el array con mis Servicios
-
-const servicios = [cambioDeAceite, cambioDePastillasDeFrenos, alineacionYBalanceo, rotacionDeNeumaticos, inspeccionComputarizada, serviceAnual15MKm];
 
 
 //Declaro el array para el carrito de compras de Servicios
@@ -36,6 +62,7 @@ if(localStorage.getItem("carritoDeServicios")) {
 }
 
 
+
 //creo el contenedor para mis cards de servicios
 
 const contenedorDeServicios = document.getElementById("contenedorDeServicios");
@@ -43,7 +70,7 @@ const contenedorDeServicios = document.getElementById("contenedorDeServicios");
 
 //Creo la Función para renderizar mis cards de Servicios
 
-const mostrarServicio = () => {
+/* const mostrarServicio = () => {
     servicios.forEach(servicio => {
         const card = document.createElement("div");
         card.classList.add("text-center", "col-xl-3", "col-md-6", "col-xs-12");
@@ -53,7 +80,7 @@ const mostrarServicio = () => {
                     <div class="card-body">
                          <h3>${servicio.tipoDeServicio}</h3>
                          <p>$${servicio.preciodeServicio}</p>
-                        <button class="btn botonServicios btn-primary" id="boton${servicio.id}">Agregar al Carrito</button>
+                        <button class="btn btn-primary" id="boton${servicio.id}">Agregar al Carrito</button>
                     </div>
                 </div>`;
         contenedorDeServicios.appendChild(card);
@@ -68,12 +95,10 @@ const mostrarServicio = () => {
             boton.styleopacity = 0.95;
         })
     })
-   
 }
-
+ */
 mostrarServicio();
 
-const botonServicios = document.querySelectorAll(`.botonServicios`);
 
 //Funcion para agregar al Carrito
 
@@ -149,7 +174,6 @@ const eliminarServicio = (id) => {
 
 //Vaciamos el carrito
 const vaciarElCarrito = document.getElementById("vaciarElCarrito");
-
 vaciarElCarrito.addEventListener("click", () => {
     eliminarCarrito ();
 })
@@ -159,11 +183,6 @@ vaciarElCarrito.addEventListener("click", () => {
 
 const eliminarCarrito = () => {
     carritoDeServicios = [];
-    botonServicios.forEach (boton => {
-        boton.disabled = false;
-        boton.styleopacity= 0;
-        });
-
 
     //Actualizo el local Storage
     localStorage.clear();
@@ -203,5 +222,5 @@ loguearseYFinalizar.addEventListener("click", () => {
     })
     }   
 })
-eliminarCarrito ();
 })
+
